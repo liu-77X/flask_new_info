0abb285875f12aabe789dd1b662843def8925f52
liu-77X <62330654+liu-77X@users.noreply.github.com>
2020-06-15 19:21:17 +0800

    数据库独立

---
+++ config.py
@@ -0,0 +1,18 @@
+import redis
+
+
+class Config(object):
+    """工程配置信息"""
+    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
+    DEBUG = True
+    # 数据库的配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:root@127.0.0.1:3306/information14"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+    # flask_session的配置信息
+    SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
+    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
+    SESSION_PERMANENT = False
+    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
--- manage.py
+++ manage.py
@@ -3,22 +3,9 @@ from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from flask_session import Session
-
-class Config(object):
-    """工程配置信息"""
-    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
-    DEBUG = True
-    # 数据库的配置信息
-    SQLALCHEMY_DATABASE_URI = "mysql://root:root@127.0.0.1:3306/information14"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-    # flask_session的配置信息
-    SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
-    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
-    SESSION_PERMANENT = False
-    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from config import Config
 
 app = Flask(__name__)
 
@@ -31,7 +18,8 @@ redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
 # 设置session保存指定位置
 Session(app)
-
+manager=Manager(app,db)
+manager.add_command('db', MigrateCommand)
 @app.route('/')
 def index():
     # 添加一个键值对
