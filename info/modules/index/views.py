c4ac9ffd6fb102c3d202bad6c77bd16cef7c9f99
liu-77X <62330654+liu-77X@users.noreply.github.com>
2020-06-22 19:37:36 +0800

    新闻首页未完成

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,45 @@
-from flask import render_template, current_app, session
+from flask import render_template, current_app, session, request, jsonify
 
 from . import index_blu
 from ... import constants
 from ...models import User, News, Category
+from ...utils.response_code import RET
 
+
+@index_blu.route('/newlist')
+def get_news_list():
+    # 获取参数
+    args_dict=request.args
+    page=args_dict.get('page',1)#第几页
+    per_page=args_dict.get('per_page',constants.HOME_PAGE_MAX_NEWS)#一夜多少数据
+    category_id=args_dict.get('cid',1)
+    # 校验参数
+    try:
+        page=int(page)
+        per_page=int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR,errsg="参数错误")
+    # 查询数据并分页
+    filters = []
+    if category_id!="1":
+        filters.append(News.category_id==category_id)
+    try:
+        pagnite=News.query.filter(*filters).order_by(News.create_time.desc()).pagnite(page, per_page, False)
+        # 获取查询的数据
+        items=pagnite.items
+        # 获取总页数
+        total_page=pagnite.pages
+        current_page=pagnite.page
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DATAERR, errsg="数据获取失败")
+    new_li=[]
+    for news in items:
+        new_li.append(news.to_basic_dict())
+    return jsonify(errno=RET.OK, errsg="OK",total_page=total_page,current_page=current_page,newsList=new_li,cid=category_id)
+
+    # 返回数据
 
 @index_blu.route('/')
 def index():
