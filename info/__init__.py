fd87a7a36c24bfb72d784221518d9c0342120407
liu-77X <62330654+liu-77X@users.noreply.github.com>
2020-06-22 18:33:24 +0800

    点击排行，进行过滤等操作

--- info/__init__.py
+++ info/__init__.py
@@ -37,7 +37,8 @@ def create_app(config_name):
     # 注册蓝图
     from info.modules.index import index_blu
     app.register_blueprint(index_blu)
-
+    from info.utils.common import do_index_class
+    app.add_template_filter(do_index_class,"index_class")
     from info.modules.passport import passport_blu
     app.register_blueprint(passport_blu)
     return app
--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, current_app, session
 
 from . import index_blu
-from ...models import User
+from ... import constants
+from ...models import User, News
 
 
 @index_blu.route('/')
@@ -11,13 +12,27 @@ def index():
     user_id=session.get("user_id")
     # 通过id查询用户信息
     user=None
+    news_list=None
     if user_id:
         try:
             user=User.query.get(user_id)
         except Exception as e:
             current_app.logger.error(e)
+    try:
+        news_list=News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+    click_news_list=[]
+    for news in news_list if news_list else []:
+        click_news_list.append(news.to_basic_dict())
+    data={
+        "user_info": user.to_dict() if user else None,
+        "click_news_list":click_news_list
+          }
 
-    return render_template('news/index.html',data={"user_info":user.to_dict() if user else None})
+    return render_template('news/index.html',data=data)
+
+
 @index_blu.route('/favicon.ico')
 def get_web_logo():
     return current_app.send_static_file('news/favicon.ico')
